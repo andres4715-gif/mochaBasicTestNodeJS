0e5f34e25a8f349c45cdb1e5d3c75659a68c94d3
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-01-05 07:55:50 -0500

    new setup get

--- src/test/apiTesting/AXIOS/get.example.test.js
+++ src/test/apiTesting/AXIOS/get.example.test.js
@@ -7,11 +7,22 @@ const axios = require("axios").default;
 // });
 
 // GET other example
+// axios({
+//   method: "get",
+//   url: "http://bit.ly/2mTM3nY",
+//   responseType: "stream",
+// }).then(function (response) {
+//   console.log(response.status);
+//   console.log(response.statusText);
+// });
+
+// GET other example with baseURL
 axios({
   method: "get",
-  url: "http://bit.ly/2mTM3nY",
-  responseType: "stream",
+  baseURL: "https://reqres.in/api",
+  url: "/users?page=1",
 }).then(function (response) {
   console.log(response.status);
   console.log(response.statusText);
+  console.log(response.data);
 });
