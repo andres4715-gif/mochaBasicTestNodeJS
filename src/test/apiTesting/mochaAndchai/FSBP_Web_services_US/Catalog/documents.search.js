f218eedabcf31f1424cf766fbb61f19f7fbbe0a9
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-01-12 13:17:29 -0500

    new fsbp api test

---
+++ src/test/apiTesting/mochaAndchai/FSBP_Web_services_US/Catalog/documents.search.js
@@ -0,0 +1,27 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+const assert = require("chai").assert;
+
+chai.use(chaiHttp);
+
+const url = "https://api-stage.firestonebpco.com";
+
+describe("Check the API status code ", () => {
+  it("should get and compare the status code", (done) => {
+    chai
+      .request(url)
+      .get(
+        "/solr/site_search/select?q=roof&fq=(country:us)&fq=(language:en)&fq=(type:assets)&rows=10&facet.field=documentType&facet=true"
+      )
+      .end(function (err, res) {
+        console.log(res.body);
+        console.log("The status code: ", res.statusCode);
+        assert.equal(
+          res.status,
+          "200",
+          "The status code is not the correct answer"
+        );
+        done();
+      });
+  });
+});
