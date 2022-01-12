cd3a24ba2ef84bd4bcd5ed42f493a2810f9402dd
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-01-12 10:22:46 -0500

    rest of elements checked

--- src/test/apiTesting/mochaAndchai/apitest.js
+++ src/test/apiTesting/mochaAndchai/apitest.js
@@ -6,6 +6,10 @@ const assert = require("chai").assert;
 chai.use(chaiHttp);
 
 const idList = [];
+const emailList = [];
+const firstNameList = [];
+const lastNameList = [];
+const avatarList = [];
 
 const url = "https://reqres.in/api/";
 
@@ -78,13 +82,14 @@ describe("Check the API status code ", () => {
       });
   });
 
-  it("should be able to check each id from json file way 1", (done) => {
+  it("should be able to check each (id) from json file way 1", (done) => {
     chai
       .request(url)
       .get("users?page=1")
       .end(function (err, res) {
         for (let i = 0; i < res.body.data.length; i++) {
           idList.push(res.body.data[i].id);
+          assert.isNumber(idList[i], "Check why the id is not a number");
           assert.isFalse(
             isNaN(!idList[i], "Check the id... it is not a number")
           );
@@ -100,9 +105,94 @@ describe("Check the API status code ", () => {
       .end(function (err, res) {
         for (let i = 0; i < res.body.data.length; i++) {
           idList.push(res.body.data[i].id);
+          assert.isNumber(idList[i], "Check why the id is not a number");
           assert.isTrue(
             typeof idList[i] === "number",
             "Check the id... it is not a number"
+          );
+        }
+        done();
+      });
+  });
+
+  it("should be able to check each (email) from json file that include (@) ", (done) => {
+    chai
+      .request(url)
+      .get("users?page=1")
+      .end(function (err, res) {
+        for (let i = 0; i < res.body.data.length; i++) {
+          emailList.push(res.body.data[i].email);
+          assert.isString(
+            emailList[i],
+            "Check why the e-mail is not formatted correctly"
+          );
+          assert.include(
+            emailList[i],
+            "@",
+            "check why the e-mail does not have an @ character"
+          );
+          assert.isNotNull(emailList[i], "Check why the e-mail is null");
+        }
+        done();
+      });
+  });
+
+  it("should be able to check each (first Name) from json file that it is not a null value and is a string", (done) => {
+    chai
+      .request(url)
+      .get("users?page=1")
+      .end(function (err, res) {
+        for (let i = 0; i < res.body.data.length; i++) {
+          firstNameList.push(res.body.data[i].first_name);
+          assert.isString(
+            firstNameList[i],
+            "Check why first_name is not a string"
+          );
+          assert.isNotNull(
+            firstNameList[i],
+            "Check why the first_name is a null value"
+          );
+        }
+        done();
+      });
+  });
+
+  it("should be able to check each (Last Name) from json file that it is not a null value and is a string", (done) => {
+    chai
+      .request(url)
+      .get("users?page=1")
+      .end(function (err, res) {
+        for (let i = 0; i < res.body.data.length; i++) {
+          lastNameList.push(res.body.data[i].last_name);
+          assert.isString(
+            lastNameList[i],
+            "Check why first_name is not a string"
+          );
+          assert.isNotNull(
+            lastNameList[i],
+            "Check why the last_name is a null value"
+          );
+        }
+        done();
+      });
+  });
+
+  it("should be able to check each (avatar) from json file that it is not a null value and is a string", (done) => {
+    chai
+      .request(url)
+      .get("users?page=1")
+      .end(function (err, res) {
+        for (let i = 0; i < res.body.data.length; i++) {
+          avatarList.push(res.body.data[i].avatar);
+          assert.isString(avatarList[i], "Check why avatar is not a string");
+          assert.isNotNull(
+            avatarList[i],
+            "Check why the avatar is a null value"
+          );
+          assert.include(
+            avatarList[i],
+            "https://",
+            "Check why the avatar not include https://"
           );
         }
         done();
