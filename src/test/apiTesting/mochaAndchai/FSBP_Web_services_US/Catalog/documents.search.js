2959f63908e4f08a74c5b8aee1f460197a662d0b
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2022-01-13 08:57:58 -0500

    response first part ready

--- src/test/apiTesting/mochaAndchai/FSBP_Web_services_US/Catalog/documents.search.js
+++ src/test/apiTesting/mochaAndchai/FSBP_Web_services_US/Catalog/documents.search.js
@@ -4,30 +4,54 @@ const assert = require("chai").assert;
 
 chai.use(chaiHttp);
 
+const params = {
+  q: "roof",
+  "facet.field": "documentType",
+  fq: ["(country:us)", "(language:en)", "(type:assets)"],
+  rows: "10",
+  facet: "true",
+};
+
 const url = "https://api-stage.firestonebpco.com";
+const endPoint = "/solr/site_search/select";
 
 describe("GET FSBP API response for Document Search  ", () => {
   it("should get and compare the status code", (done) => {
     chai
       .request(url)
-      .get("/solr/site_search/select")
-      .query({
-        q: "roof",
-        fq: "country:us",
-        fq: "language:en",
-        fq: "type:assets",
-        rows: 10,
-        "facet.field": "documentType",
-        facet: true,
-      })
+      .get(endPoint)
+      .query(params)
       .end(function (err, res) {
-        console.log(res.body);
-        console.log("The status code: ", res.statusCode);
         assert.equal(
           res.status,
           "200",
           "The status code is not the correct answer"
         );
+        done();
+      });
+  });
+
+  it("should get the response and compare de element object", (done) => {
+    chai
+      .request(url)
+      .get(endPoint)
+      .query(params)
+      .end(function (err, res) {
+        const response = [
+          res.body.response.numFound,
+          res.body.response.start,
+          res.body.response.maxScore,
+        ];
+
+        for (let i = 0; i < response.length; i++) {
+          assert.typeOf(
+            response[i],
+            "number",
+            "Check the numFound it is not a number value"
+          );
+          assert.isNotNull(response[i], "Check the numFound must not be null");
+          assert.isTrue(response[i] >= 0);
+        }
         done();
       });
   });
