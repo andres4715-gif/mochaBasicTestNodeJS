b25c43a68998fc38b263214e7d01948ea902eec2
andres rios <andres4715@hotmail.es>
2022-01-21 18:00:49 -0500

    new changes from windows

--- src/test/palindrome.test.js
+++ src/test/palindrome.test.js
@@ -2,6 +2,7 @@ var Palindrome = require("./palindrome/palindrome.js");
 var Palindrome3 = require("./palindrome/palindrome3.js");
 var Palindrome4 = require("./palindrome/palindrome4");
 var Palindrome5 = require("./palindrome/palindrome5");
+var Palindrome6 = require("./palindrome/palindrome6");
 
 describe("Practicing Palindrome", function () {
   it("Getting palindrome results", function () {
@@ -9,5 +10,6 @@ describe("Practicing Palindrome", function () {
     console.log(Palindrome3.palindromeTest3());
     console.log(Palindrome4.Palindrome4("123329"));
     console.log(Palindrome5.checkPalindrome5("123321"));
+    console.log(Palindrome6.checkPalindrome6("123321"));
   });
 });
